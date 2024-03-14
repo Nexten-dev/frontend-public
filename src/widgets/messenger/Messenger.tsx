d5d8ce2612eec089696c68e9f7eeab448d28987d
Вячеслав Воробьев <slavavorobey@vk.com>
2024-03-14 22:13:56 +0700

    add base layout

--- src/widgets/messenger/Messenger.tsx
+++ src/widgets/messenger/Messenger.tsx
@@ -1,17 +1,63 @@
-import styles from './styles.module.scss';
+'use client'
+import IconArrowDown from '@/shared/ui/icons/icon-arrow/IconArrowDown'
+import IconUpdate from '@/shared/ui/icons/icon-update/IconUpdate'
+import { useState } from 'react'
+import styles from './styles.module.scss'
 
 const Messenger = () => {
-  return (
-    <div className={styles.block}>
-      <p>
-        Тут мессенджер со списком контактов слева и с детальным чатом справа
-      </p>
-      <p>
-        Плюс куча возможностей в чате, по типу аудио-/видеозвонка, добавление в ЧС, отправка голосовых, эмодзи, картиннок, текста в три страницы а4, плюс всё это будет отмечаться датами и временем. 
-      </p>
-      <p>Можно спалить когда контакт был онлайн, а можно не спалить, если он скрывается</p>
-    </div>
-  );
-};
+	const [activeButton, setActiveButton] = useState('primary')
 
-export default Messenger;
+	const handleButtonClick = (buttonName: string) => {
+		setActiveButton(buttonName)
+	}
+	return (
+		<div className={styles.block}>
+			<div className={styles.block__left}>
+				<div className={styles.block__left_top}>
+					<form className={styles.block__form}>
+						<select
+							name='update__list'
+							id='update__list'
+							className={styles.block__select_top}
+						>
+							<option value='upvox'>upvox_</option>
+							<option value='upvox1'>upvox_1</option>
+							<option value='upvox2'>upvox_2</option>
+						</select>
+						<label htmlFor='update__list' className={styles.block__input}>
+							<IconArrowDown />
+						</label>
+					</form>
+
+					<button className={`btn-reset ${styles.block__btn_update}`}>
+						<IconUpdate />
+					</button>
+				</div>
+				<div className={styles.block__left_actions}>
+					<button
+						className={`btn-reset ${styles.block__btn_actions} ${
+							activeButton === 'primary' ? styles.block__btn_actions_active : ''
+						}`}
+						onClick={() => handleButtonClick('primary')}
+					>
+						Primary
+					</button>
+					<button
+						className={`btn-reset ${styles.block__btn_actions} ${
+							activeButton === 'general' ? styles.block__btn_actions_active : ''
+						}`}
+						onClick={() => handleButtonClick('general')}
+					>
+						General
+					</button>
+				</div>
+				<div className={styles.block__left_users}></div>
+			</div>
+			<div className={styles.block__right}>
+				<div className={styles.block__right_top}></div>
+			</div>
+		</div>
+	)
+}
+
+export default Messenger

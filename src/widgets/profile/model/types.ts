2bba840b7fef376189c52b62e9215db0dc535dd9
permyakov83 <permyakov83@gmail.com>
2024-03-17 21:57:12 +0500

    17-03-2024

--- src/widgets/profile/model/types.ts
+++ src/widgets/profile/model/types.ts
@@ -1,4 +1,5 @@
 export type PropsStory = {
   imageSource: { src: string; height?: number; width?: number };
   description: string;
+  loock: boolean;
 };
--- src/widgets/profile/ui/Panel.tsx
+++ src/widgets/profile/ui/Panel.tsx
@@ -1,9 +1,42 @@
+import avatar from '@assets/avatar.jpg';
 import styles from '../styles.module.scss';
+
+const user = {
+  nickname: 'vasya',
+};
 
 const Panel = () => {
   return (
     <div className={styles.panel}>
-      Панелька со всей инфой о пользователе
+      <div className={styles.panel__avatar}>
+        <div className={styles.avatar}>
+          <div
+            className={styles.avatar__url}
+            style={{ backgroundImage: `url(${avatar.src})` }}
+          ></div>
+        </div>
+      </div>
+      <div className={styles.panel__info}>
+        <div className={styles.panel__action}>
+          <span className={styles.panel__nickname}>{user.nickname}</span>
+          <ul className={`list-reset ${styles['panel__list-btn']}`}>
+            <li className={styles['panel__item-btn']}>
+              <button className={`btn-reset ${styles.panel__following}`}>
+                Following
+              </button>
+            </li>
+            <li className={styles['panel__item-btn']}>
+              <button className={`btn-reset ${styles.panel__message}`}>
+                Message
+              </button>
+            </li>
+            <li className={styles['panel__item-btn']}>
+              <button className={`btn-reset ${styles.panel__add}`}></button>
+            </li>
+          </ul>
+          <button className={`btn-reset ${styles.panel__burger}`}></button>
+        </div>
+      </div>
     </div>
   );
 };

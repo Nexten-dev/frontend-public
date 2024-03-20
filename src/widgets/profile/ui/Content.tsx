b19cfd9f3aa1bd567a6e3fe55ec61062887a5221
permyakov83 <permyakov83@gmail.com>
2024-03-20 16:41:41 +0500

    20-03-2024

--- src/widgets/profile/ui/Content.tsx
+++ src/widgets/profile/ui/Content.tsx
@@ -1,12 +1,38 @@
 import Image from 'next/image';
+import post1 from '@assets/post1.jpg';
 import styles from '../styles.module.scss';
 
 const Content = () => {
   return (
     <div className={styles.content}>
-      <button className={styles.content__posts}>
-        <Image src='@assets/posts-icon.svg' alt='posts'></Image>
-      </button>
+      <ul className={`list-reset ${styles['btn-list']}`}>
+        <li className={styles['btn-item']}>
+          <button
+            className={`btn-reset ${styles['btn-posts']} ${styles['content__btn']} ${styles['content__btn--active']}`}
+          >
+            POSTS
+          </button>
+        </li>
+        <li className={styles['btn-item']}>
+          <button
+            className={`btn-reset ${styles['btn-reels']} ${styles['content__btn']}`}
+          >
+            REELS
+          </button>
+        </li>
+        <li className={styles['btn-item']}>
+          <button
+            className={`btn-reset ${styles['btn-tagged']} ${styles['content__btn']}`}
+          >
+            TAGGED
+          </button>
+        </li>
+      </ul>
+      <div className={styles['content__posts']}>
+        <Image src={post1} alt='post1'></Image>
+        <Image src={post1} alt='post1'></Image>
+        <Image src={post1} alt='post1'></Image>
+      </div>
     </div>
   );
 };
--- src/widgets/profile/ui/Posts.tsx
+++ src/widgets/profile/ui/Posts.tsx
@@ -5,9 +5,6 @@ import styles from '../styles.module.scss';
 const Posts = () => {
   return (
     <div className={styles.posts}>
-      Тут посты, рилсы и тагеты (чтобы это ни значило)
-      <br />
-      По идее кнопки-табы ещё, чтоб совсем красиво было
       <div className={styles.content}>
         <button className={`btn-reset ${styles.content__posts}`}>POSTS</button>
       </div>

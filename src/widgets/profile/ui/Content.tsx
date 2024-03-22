0c6430f95aa89e62d2ffb482dcbaa0486f4bffed
permyakov83 <permyakov83@gmail.com>
2024-03-22 11:28:10 +0500

    add: Panel, Content components. upd: style.scss file

--- src/widgets/profile/ui/Content.tsx
+++ src/widgets/profile/ui/Content.tsx
@@ -3,33 +3,8 @@ import post1 from '@assets/post1.jpg';
 import post2 from '@assets/post2.jpeg';
 import post3 from '@assets/post3.jpeg';
 import styles from '../styles.module.scss';
-/*
-let tabBtn = document.querySelectorAll('.how__link-step');
-let tabItem = document.querySelectorAll('.how__content');
-
-tabBtn.forEach(function (elem) {
-  elem.addEventListener('click', function (e) {
-    const path = e.currentTarget.dataset.path;
-
-    tabBtn.forEach(function (btn) {
-      btn.classList.remove('how__link-step_active');
-    });
-
-    e.currentTarget.classList.add('how__link-step_active');
-
-    tabItem.forEach(function (element) {
-      element.classList.remove('how__content_active');
-    });
-
-    document
-      .querySelector(`[data-target="${path}"]`)
-      .classList.add('how__content_active');
-  });
-});
-*/
 
 const Content = () => {
-  let btnAction = document.querySelectorAll('.');
   return (
     <div className={styles.content}>
       <ul className={`list-reset ${styles['btn-list']}`}>
@@ -62,7 +37,7 @@ const Content = () => {
         className={`list-reset ${styles['content__list']} ${styles['content__list--active']}`}
         data-target='posts'
       >
-        <li className={styles['content__item']} style={{ height: 342 }}>
+        <li className={styles['content__item']}>
           <button className={`btn-reset ${styles['content__view']}`}>
             <Image
               src={post1}
@@ -72,7 +47,7 @@ const Content = () => {
             ></Image>
           </button>
         </li>
-        <li className={styles['content__item']} style={{ height: 342 }}>
+        <li className={styles['content__item']}>
           <button className={`btn-reset ${styles['content__view']}`}>
             <Image
               src={post2}
@@ -82,7 +57,7 @@ const Content = () => {
             ></Image>
           </button>
         </li>
-        <li className={styles['content__item']} style={{ height: 342 }}>
+        <li className={styles['content__item']}>
           <button className={`btn-reset ${styles['content__view']}`}>
             <Image
               src={post3}
@@ -92,71 +67,11 @@ const Content = () => {
             ></Image>
           </button>
         </li>
-        <li className={styles['content__item']} style={{ height: 342 }}>
+        <li className={styles['content__item']}>
           <button className={`btn-reset ${styles['content__view']}`}>
             <Image
               src={post1}
               alt='post1'
-              fill={true}
-              objectFit='cover'
-            ></Image>
-          </button>
-        </li>
-      </ul>
-      <ul
-        className={`list-reset ${styles['content__list']}`}
-        data-target='reels'
-      >
-        <li className={styles['content__item']} style={{ height: 342 }}>
-          <button className={`btn-reset ${styles['content__view']}`}>
-            <Image
-              src={post1}
-              alt='post1'
-              fill={true}
-              objectFit='cover'
-            ></Image>
-          </button>
-        </li>
-        <li className={styles['content__item']} style={{ height: 342 }}>
-          <button className={`btn-reset ${styles['content__view']}`}>
-            <Image
-              src={post1}
-              alt='post1'
-              fill={true}
-              objectFit='cover'
-            ></Image>
-          </button>
-        </li>
-        <li className={styles['content__item']} style={{ height: 342 }}>
-          <button className={`btn-reset ${styles['content__view']}`}>
-            <Image
-              src={post1}
-              alt='post1'
-              fill={true}
-              objectFit='cover'
-            ></Image>
-          </button>
-        </li>
-      </ul>
-      <ul
-        className={`list-reset ${styles['content__list']}`}
-        data-target='tagged'
-      >
-        <li className={styles['content__item']} style={{ height: 342 }}>
-          <button className={`btn-reset ${styles['content__view']}`}>
-            <Image
-              src={post1}
-              alt='post1'
-              fill={true}
-              objectFit='cover'
-            ></Image>
-          </button>
-        </li>
-        <li className={styles['content__item']} style={{ height: 342 }}>
-          <button className={`btn-reset ${styles['content__view']}`}>
-            <Image
-              src={post2}
-              alt='post2'
               fill={true}
               objectFit='cover'
             ></Image>
